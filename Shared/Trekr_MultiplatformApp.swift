0e2de60b76c4b07812a0209d946565099ce735a1
Devon Hansen <dhansen@lcs.on.ca>
2022-01-18 09:25:38 -0500

    Initial Commit

---
+++ Shared/Trekr_MultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trekr_MultiplatformApp.swift
+//  Shared
+//
+//  Created by Devon Kenneth Hansen on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Trekr_MultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
