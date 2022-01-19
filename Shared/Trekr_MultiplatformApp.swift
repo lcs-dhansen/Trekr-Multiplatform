9675307f9c1fc4a84ee9856aa247113225b2b7d7
Devon Hansen <dhansen@lcs.on.ca>
2022-01-19 10:49:17 -0500

    Added Tips and map view and fixed a syntax error

--- Shared/Trekr_MultiplatformApp.swift
+++ Shared/Trekr_MultiplatformApp.swift
@@ -9,10 +9,37 @@ import SwiftUI
 
 @main
 struct Trekr_MultiplatformApp: App {
+    
+    @StateObject var store = LocationStore()
+    
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                LocationDetail(location: Location.example)
+            
+            TabView {
+                
+                NavigationView {
+                    LocationsList(store:store)
+                }
+                .tabItem{
+                    Image(systemName: "list.bullet")
+                    Text("Locations")
+                }
+                
+                NavigationView {
+                   WorldMap()
+                }
+                .tabItem{
+                    Image(systemName: "map")
+                    Text("Map")
+                }
+                
+                NavigationView {
+                    TipsList()
+                }
+                .tabItem{
+                    Image(systemName: "person.fill.questionmark")
+                    Text("Tips")
+                }
             }
         }
     }
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  Trekr Multiplatform
+//
+//  Created by Devon Kenneth Hansen on 2022-01-19.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("Hello, WorldMap View!")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
