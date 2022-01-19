550f1d344fef6bae0a8c91c7203ca68ebff013dc
Devon Hansen <dhansen@lcs.on.ca>
2022-01-19 10:09:35 -0500

    Added location store to pull information from the JSON url

---
+++ Shared/Data Model/LocationStore.swift
@@ -0,0 +1,30 @@
+//
+//  LocationStore.swift
+//  Trekr Multiplatform
+//
+//  Created by Devon Kenneth Hansen on 2022-01-19.
+//
+
+import Foundation
+
+class LocationStore: ObservableObject {
+    
+    var places: [Location]
+    
+    init() {
+        
+        // get a pointer to the file
+        let url = Bundle.main.url(forResource: "location", withExtension: "json")!
+        
+        //load the contents of the JSON file
+        let data = try! Data(contentsOf: url)
+        
+        // convert the data from the JSON file into the array
+        places = try! JSONDecoder().decode([Location].self, from: data)
+        
+        //sort the list of locations in alphabetical order by name ascending
+        places.sort(by: {
+            $0.name < $1.name
+        })
+    }
+}
