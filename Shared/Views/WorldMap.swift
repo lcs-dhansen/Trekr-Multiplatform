9ad22daebc69b6531b410f3b265ebee34d011b86
Devon Hansen <dhansen@lcs.on.ca>
2022-01-19 10:53:22 -0500

    Imported map kit and started setup for map view. End of class Jan 19

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -5,11 +5,18 @@
 //  Created by Devon Kenneth Hansen on 2022-01-19.
 //
 
+import MapKit
 import SwiftUI
 
 struct WorldMap: View {
+    
+    //Centre on LCS, wide enough to show ost of eastern North AMerica
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.26571), span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
+    
     var body: some View {
-        Text("Hello, WorldMap View!")
+        
+        Map(coordinateRegion: $region)
+            .navigationTitle(Map)
     }
 }
 
